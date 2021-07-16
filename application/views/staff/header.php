5fab5c4f1bb067d83c2d9e07341d1a5aa73c2c1f
SUasyraaf <asyraaf_08@yahoo.com>
2021-07-17 03:02:02 +0800

    remove search box

--- application/views/staff/header.php
+++ application/views/staff/header.php
@@ -93,34 +93,12 @@
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-      <!-- /.search form -->
+
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
 
         <li class="active"><a href="<?php echo base_url(); ?>"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-
-        <!-- <li class="treeview">
-          <a href="#">
-            <i class="fa fa-usd"></i> <span>Salary</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url(); ?>view-salary"><i class="fa fa-circle-o"></i> View Salary</a></li>
-          </ul>
-        </li> -->
 
         <li class="treeview">
           <a href="#">
